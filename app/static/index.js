c4d6ea2946d651b111b647818fb218f6c373b7af
64617 <45166288+64617@users.noreply.github.com>
2022-09-21 09:59:53 +0800

    add derpi tag list on page, among other things

--- app/static/index.js
+++ app/static/index.js
@@ -5,18 +5,34 @@ const error_e = document.getElementById('error-desc')
 const submit_e = document.getElementById('submit-button')
 const wc_elem = document.getElementById('word-count')
 const ts_elem = document.getElementById('timestamp')
+const tags_e = document.getElementById('tag-dump')
 const id = +id_elem.innerHTML
 
+function appendtext(s) {
+	desc_elem.innerHTML += s+' ';
+	checkWordLen()
+}
+function addtag(s) {
+	const linked_tag = document.createElement('a')
+	linked_tag.href = '#'
+	linked_tag.innerHTML = s;
+	linked_tag.onclick = e => {
+		e.preventDefault()
+		appendtext(s)
+	}
+	tags_e.appendChild(linked_tag)
+	linked_tag.insertAdjacentHTML('afterend', '&nbsp;')
+}
 async function showImage() {
 	try {
   const res = await fetch(`https://derpibooru.org/api/v1/json/images/${id}`)
 	const json = await res.json();
 	const url = json.image.representations.medium;
 	im_elem.onload = () => {
-		desc.style.width = im_elem.width;
+		tags_e.style.maxWidth = desc.style.width = im_elem.width;
 	}
 	im_elem.src = url;
-	console.log(url)
+	json.image.tags.forEach(s => addtag(s))
 	} catch (e) {
 		console.log(e)
 		error_e.innerHTML = 'Error (refresh the page):'+e;
