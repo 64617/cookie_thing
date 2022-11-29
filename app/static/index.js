148f01d8c9199ebbc6edd1937d5f41858417a9af
64617 <45166288+64617@users.noreply.github.com>
2022-11-29 09:46:17 +0800

    i changed the entire js file at some point

--- app/static/index.js
+++ app/static/index.js
@@ -1,3 +1,30 @@
+const _libs = {
+  JSZip: 'https://cdnjs.cloudflare.com/ajax/libs/jszip/3.10.1/jszip.min.js',
+	//FileSaver: 'https://cdnjs.cloudflare.com/ajax/libs/FileSaver.js/2.0.5/FileSaver.min.js'
+	FileSaver: '/static/FileSaver.js'
+}
+async function use(name) {
+	if (typeof _libs[name] === "string")
+		_libs[name] = await import(_libs[name])
+	return _libs[name]
+}
+
+function tempSpinner(needsSpinner, promise) {
+	const spinner = document.createElement('div')
+	spinner.className = 'spinner'
+	spinner.id = 'temp-spinner'
+	old_inner = needsSpinner.innerHTML;
+	needsSpinner.innerHTML = "";
+	needsSpinner.appendChild(spinner);
+	needsSpinner.disabled = true;
+	return promise.finally(res => {
+		needsSpinner.removeChild(spinner);
+		needsSpinner.innerHTML = old_inner;
+		needsSpinner.disabled = false;
+		return res;
+	})
+}
+
 function sanitizeTagList(s) {
 	return s
 		.replace(/\s+/g,' ') // remove extra whitespaces
@@ -5,7 +32,6 @@ function sanitizeTagList(s) {
 		.replace(/^,|,$/g,'')  // extra: remove trailing/starting commas
 		.replaceAll(', ', ',');
 }
-  
 
 const im_elem = document.getElementById('derpi-img')
 const id_elem = document.getElementById('derpi-id')
@@ -22,6 +48,10 @@ const custom_filter_toggle_e = document.getElementById('filter-menu-toggle')
 const superlist_e = document.getElementById('superlist')
 const whitelist_e = document.getElementById('whitelist')
 const blacklist_e = document.getElementById('blacklist')
+const export_button_e = document.getElementById('button-export-personal')
+const export_include_images_e = document.getElementById('export-include-images')
+const export_use_filter_e = document.getElementById('export-use-filter')
+const export_min_len_e = document.getElementById('export-min-len')
 const id = +id_elem.innerHTML
 let char_set;
 
@@ -136,11 +166,14 @@ async function get_characters() {
 	}
 	return localStorage.getItem('char_list').split('\n')
 }
+async function derpi_image_info(id) {
+  const res = await fetch(`https://derpibooru.org/api/v1/json/images/${id}`)
+	return await res.json();
+}
 async function showImage() {
 	try {
 	char_set = new Set(await get_characters())
-  const res = await fetch(`https://derpibooru.org/api/v1/json/images/${id}`)
-	const json = await res.json();
+	const json = await derpi_image_info(id);
 	const url = json.image.representations.medium;
 	im_elem.onload = () => {
 		tags_e.style.maxWidth = desc.style.width = im_elem.width;
@@ -153,7 +186,7 @@ async function showImage() {
 		error_e.hidden = false
 	}
 }
-showImage();
+//showImage();
 
 function checkWordLen() {
 	const s = desc_elem.value.trim()
@@ -173,3 +206,153 @@ function checkWordLen() {
 desc_elem.addEventListener('input', checkWordLen)
 
 ts_elem.value = (Date.now()/1000)|0;
+
+async function get_captions(use_filter) {
+	const res = await fetch(`/api/collect/${+use_filter}`, {
+		credentials: 'include'
+	})
+	const json = await res.json();
+	return json
+}
+async function derpi_image_dl(id) {
+	const json = await derpi_image_info(id);
+	const full_url = json.image.representations.full;
+	const res = await fetch(full_url);
+	return [await res.blob(), full_url.split('/').pop()];
+}
+function validate_prompt(p_text, min_words) {
+	// Make sure prompts aren't too long
+	if (p_text.length > 250) {
+		console.log("PRUNING:", p_text);
+		// look for the last '.' within limit.
+		let i;
+		for (i = 250; i > 0; i--) {
+			if (p_text[i] === '.') break;
+		}
+    p_text = p_text.slice(0,i);
+	}
+	return p_text.split(' ').length < min_words ?
+		'' : p_text;
+}
+
+async function add_images_to_zip(captions) {
+	await use("JSZip"); // cursed
+	const zip = JSZip(); // no new because ????
+	for (const [id,p_text] of captions) {
+		const [im, fname] = await derpi_image_dl(id);
+		const ext = fname.split('.').pop();
+		zip.file(`${p_text}.${ext}`, im);
+	}
+	return zip;
+}
+async function export_captions(grab_images, use_filter, min_words) {
+	await use("FileSaver"); // cursed
+	const captions = (await get_captions(use_filter))
+		.map(([id,p_text]) => [id,validate_prompt(p_text,min_words)])
+		.filter(t => t[1]);
+
+	if (grab_images) {
+		const zip = await add_images_to_zip(captions);
+		const outfile = await zip.generateAsync({type: 'blob'});
+		saveAs(outfile, 'mycaptions.zip');
+	}
+	const outfile = new Blob(
+		[JSON.stringify(captions)],
+	  {'type': 'application/json'}
+	);
+	return saveAs(outfile, 'mycaptions.json');
+}
+export_button_e.addEventListener('click', () => {
+	export_button_e.value = 'downloading...'
+	const p = export_captions(
+		export_include_images_e.checked,
+		export_use_filter_e.checked,
+		+export_min_len_e.value,
+	);
+	tempSpinner(export_button_e, p)
+		.then(() => export_button_e.value = 'done!')
+})
+
+// modal
+function model_content(title, body_id) {
+	const modal_body = document.getElementById(body_id);
+	modal_body.hidden = false;
+	document.body.removeChild(modal_body);
+	//
+	const content = document.createElement('div');
+	content.className = "modal-content"
+	//
+	const header = document.createElement('div');
+	header.className = "modal-header"
+	const h5 = document.createElement('h5');
+	h5.className = "modal-title";
+	h5.innerHTML = title;
+	header.appendChild(h5);
+	//
+	const body = document.createElement('div');
+	body.className = "modal-body";
+	body.appendChild(modal_body);
+	//
+	const footer = document.createElement('div');
+	footer.className = "modal-footer"
+	const button = document.createElement('button');
+	button.id = "close"
+	button.className = "ml-auto btn btn-secondary";
+	button.innerHTML = 'Close'
+	button.onclick = () => modal_disable(body_id);
+	footer.appendChild(button);
+	//
+	content.appendChild(header);
+	content.appendChild(body);
+	content.appendChild(footer);
+	return content;
+}
+function modal_enable(title, body_id) {
+	const root = document.createElement('div')
+	root.id = 'modal-root'
+	root.tabIndex = -1
+	root.style = "position: relative; z-index: 1050; display: block;"
+	//
+	const first = document.createElement('div');
+	first.className = "modal fade show";
+	first.style = "display: block;";
+	first.onclick = () => modal_disable(body_id);
+	first.setAttribute('role', 'dialog');
+	first.role = "dialog";
+	first.tabIndex = -1;
+	//
+	const inner = document.createElement('div');
+	inner.onclick = e => e.stopPropagation();
+	inner.id = "modal";
+	inner.setAttribute('role', 'document');
+	inner.className = "modal-dialog";
+	//inner.style = "display: block;"
+	//inner.tabIndex = -1;
+	inner.appendChild(model_content(title, body_id));
+	//
+	first.appendChild(inner);
+	const second = document.createElement('div');
+	second.className = "modal-backdrop fade show"
+	//
+	const preroot = document.createElement('div')
+	preroot.className = "";
+	preroot.appendChild(first);
+	preroot.appendChild(second);
+	root.append(preroot)
+	//
+	document.body.appendChild(root);
+}
+function modal_disable(body_id) {
+	const modal_body = document.getElementById(body_id);
+	modal_body.hidden = true;
+	const root = document.getElementById('modal-root')
+	document.body.removeChild(root);
+	document.body.appendChild(modal_body);
+}
+
+const export_link_e = document.getElementById('open-export');
+export_link_e.addEventListener('click', () => {
+	modal_enable('Export captions', 'modal-body-export')
+});
+
+
